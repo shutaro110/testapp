f61a6fa5c62f0acb4e8a4162cf709451cb9ae39e
shutaro110 <dc.29g.7701@s.thers.ac.jp>
2024-05-01 11:52:51 +0900

    kadai02

--- app/src/main/java/jp/ac/gifu/myapplication/MainActivity.java
+++ app/src/main/java/jp/ac/gifu/myapplication/MainActivity.java
@@ -3,12 +3,22 @@ package jp.ac.gifu.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "アプリを終了しました", Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
